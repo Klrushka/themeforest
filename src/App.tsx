adc7dfeaacd42bdba4455aeb0a3a75f41ef0dc75
Kiryl Sliaptsou <kirillsleptsov12082003@gmail.com>
2022-12-08 17:05:18 +0300

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -12,10 +13,12 @@ import { store } from './store';
 export const App: React.FC = () => {
   const theme = getTheme(Themes.LIGHT);
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <MainPage />
-      </ThemeProvider>
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <MainPage />
+        </ThemeProvider>
+      </Provider>
+    </HashRouter>
   );
 };
